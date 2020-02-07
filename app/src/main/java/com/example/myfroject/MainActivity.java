d48dc8b0cff3d108f66137076642d0e72d1b6544
cecesopian <checesopian@gmail.com>
2020-02-07 09:50:45 +0700

    update pertama kali 1. mengubah main activity 2. menambah warna abu abu dengan kode #708090 3. mengubah activity main

--- app/src/main/java/com/example/myfroject/MainActivity.java
+++ app/src/main/java/com/example/myfroject/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.myfroject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -12,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int a = 10;
+
+        Button btnpindah = findViewById(R.id._btn);
+        btnpindah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Acc.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
