53946591b34321e7c122f925201cc7aa138a11c3
cecesopian <checesopian@gmail.com>
2020-03-05 14:10:03 +0700

    memasukkan file layout ktp

---
+++ app/src/main/java/com/example/myfroject/Layout_KTP.java
@@ -0,0 +1,14 @@
+package com.example.myfroject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Layout_KTP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout__ktp);
+    }
+}
